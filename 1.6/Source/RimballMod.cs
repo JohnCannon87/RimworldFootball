d3696718238203191269f0aa80ce0a4aa36407a1
John Cannon <john.arc.cannon@gmail.com>
2025-07-10 17:51:49 +0100

    Recompile for 1.6 add dds textures

---
+++ 1.6/Source/RimballMod.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using UnityEngine;
+
+namespace Rimball
+{
+    class RimballMod : Mod
+    {
+        public static RimballSettings settings;
+        private Vector2 scrollPosition = Vector2.zero;
+
+        public RimballMod(ModContentPack content) : base(content)
+        {
+            LongEventHandler.ExecuteWhenFinished(GetSettings);
+            settings = GetSettings<RimballSettings>();
+        }
+
+        public void GetSettings()
+        {
+            GetSettings<RimballSettings>();
+        }
+
+        public static IEnumerable<ThingDef> PossibleThingDefs()
+        {
+            return from d in DefDatabase<ThingDef>.AllDefs
+                   where (d.category == ThingCategory.Item && d.scatterableOnMapGen && !d.destroyOnDrop && !d.MadeFromStuff)
+                   select d;
+        }
+
+        public override void DoSettingsWindowContents(Rect rect)
+        {
+            settings.DoWindowContents(rect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Rim Ball";
+        }
+    }
+}
---
+++ 1.6/Source/RimballSettings.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using UnityEngine;
+
+namespace Rimball
+{
+    class RimballSettings : ModSettings
+    {
+        internal static bool drawPlayerSpots = false;
+        public string traitGainChance = "0.01";
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+
+            Scribe_Values.Look(ref drawPlayerSpots, "drawPlayerSpots", false);
+            Scribe_Values.Look(ref traitGainChance, "traitGainChance", "0.01");
+        }
+
+        public void DoWindowContents(Rect rect)
+        {
+            Listing_Standard list = new Listing_Standard()
+            {
+                ColumnWidth = rect.width
+            };
+
+            list.Begin(rect);
+
+            list.Label("Trait Gain Chance (0.0 to 1.0):");
+            traitGainChance = list.TextEntry(traitGainChance);
+
+            // Parse and validate the input as a float
+            if (!float.TryParse(traitGainChance, out float parsedChance) || parsedChance < 0f || parsedChance > 1f)
+            {
+                traitGainChance = "0.01";  // Reset to a default value if parsing fails
+                Log.Error("Invalid input for trait gain chance. Value must be between 0.0 and 1.0.");
+            }
+
+            list.Label("Rimball Debug Settings");
+            list.CheckboxLabeled("Draw Player Spots", ref drawPlayerSpots);
+
+            list.End();
+        }
+    }
+}
